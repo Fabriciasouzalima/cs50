5cda65ecc16bac1f46940704a70a0f3ff1674f9b
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2020-11-04 22:31:40 -0300

    functions and loops

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+houses = {
+    "Harry": "Griffindor",
+    "Draco": "Slytherin",
+    "Luna" : "Hufflepuff"
+}
+
+houses["Hermione"] = "Griffindor"
+
+print(houses["Harry"])
+print(houses["Hermione"])
---
+++ loops.py
@@ -0,0 +1,13 @@
+for i in range(6):
+    print(i)
+
+for a in [0,1,2,3,4,5]:
+    print(a)
+
+names = ["Harry", "Rony", "Hermione"]
+for name in names:
+    print(name)
+
+actor = "Daniel"
+for character in actor:
+    print(character)
